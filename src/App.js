c96606ac1c5166a325a0fa3b80d564c9e5540240
kayweba <under4e98@mail.ru>
2020-02-04 10:28:20 +0300

    Final version of the project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './css/App.css';
+import { Switch, Route } from 'react-router-dom';
+import Main from './pages';
+import Login from './pages/Login';
+import ChoosePlan from './pages/ChoosePlan';
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/login" component={Login} />
+        <Route path="/choose-plan" component={ChoosePlan} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
